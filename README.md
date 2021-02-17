100dafcd3f2c7bb25c41d74eb7d2c0e306151ea8
bartoszgurgul <bartoszgurgul@gmail.com>
2021-02-17 23:29:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Projekt przedsatwia mozliwosci Thymeleaf
+oraz aplikacje ktora przedstawpattern dla wielu krajow 
+
---
+++ src/main/java/pl/javastart/springmvcthymeleafmore/model/Book.java
@@ -0,0 +1,66 @@
+package pl.javastart.springmvcthymeleafmore.model;
+
+import java.util.Objects;
+
+public class Book {
+    private String author;
+    private String title;
+    private double price;
+
+    public Book(String author, String title, double price) {
+        this.author = author;
+        this.title = title;
+        this.price = price;
+    }
+
+    public Book() {
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "author='" + author + '\'' +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Double.compare(book.price, price) == 0 &&
+                Objects.equals(author, book.author) &&
+                Objects.equals(title, book.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(author, title, price);
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
